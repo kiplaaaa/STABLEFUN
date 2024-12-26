e5f75cd3f92a9c36d7d04982fd729dc9a3f184f7
Ge0frey <jeffkimzyjoe@gmail.com>
2024-12-26 05:20:20 +0300

    provides the context for wallet integration into the application

---
+++ src/context/WalletContextProvider.tsx
@@ -0,0 +1,21 @@
+import { FC, ReactNode, useMemo } from 'react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+const network = WalletAdapterNetwork.Devnet;
+const endpoint = 'https://api.devnet.solana.com';
+
+export const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
