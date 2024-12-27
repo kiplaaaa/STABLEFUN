b46ecf7d89cc83cab75b58a44cc60de6072c9ebc
Ge0frey <jeffkimzyjoe@gmail.com>
2024-12-27 05:09:27 +0300

    import updated modules

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
+import './polyfills';
+import { Buffer } from 'buffer';
+window.Buffer = Buffer;
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
 import App from './App.tsx';
 import { WalletContextProvider } from './context/WalletContextProvider';
 import './index.css';
+import 'util';
+import 'http-browserify';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
