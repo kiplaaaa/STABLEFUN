ce52e6aad3bdbc9d6c141b8a30da50bf8c66cedb
Ge0frey <jeffkimzyjoe@gmail.com>
2024-12-27 05:06:12 +0300

    Remove PhantomWalletAdapter import since it's now standard

--- src/context/WalletContextProvider.tsx
+++ src/context/WalletContextProvider.tsx
@@ -2,14 +2,13 @@ import { FC, ReactNode, useMemo } from 'react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
 import '@solana/wallet-adapter-react-ui/styles.css';
 
 const network = WalletAdapterNetwork.Devnet;
 const endpoint = 'https://api.devnet.solana.com';
 
 export const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+  const wallets = useMemo(() => [], []);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
