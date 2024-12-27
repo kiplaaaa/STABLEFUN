2d489a9ce6dbb3553c509bf13895d1ad3854601b
Ge0frey <jeffkimzyjoe@gmail.com>
2024-12-27 05:07:11 +0300

    Add more configs to vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,26 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      stream: 'stream-browserify',
+      buffer: 'buffer',
+      util: 'util',
+      http: 'http-browserify'
+    }
+  },
+  optimizeDeps: {
+    include: ['buffer', 'util']
+  },
+  define: {
+    global: 'globalThis',
+    'process.env': {},
+    process: {
+      env: {},
+      browser: true,
+      version: '',
+      versions: {}
+    },
+    Buffer: ['buffer', 'Buffer']
+  }
 })
