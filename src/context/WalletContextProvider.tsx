ce29e77c671b38de0a46a950565aac627117029b
Ge0frey <jeffkimzyjoe@gmail.com>
2024-12-30 01:12:49 +0300

    add solflare wallet adapters for a better user experience

--- src/context/WalletContextProvider.tsx
+++ src/context/WalletContextProvider.tsx
@@ -3,12 +3,19 @@ import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import '@solana/wallet-adapter-react-ui/styles.css';
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
 
 const network = WalletAdapterNetwork.Devnet;
 const endpoint = 'https://api.devnet.solana.com';
 
 export const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  const wallets = useMemo(() => [], []);
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+    ],
+    []
+  );
 
   return (
     <ConnectionProvider endpoint={endpoint}>
