f0d712f3d97e2c0e939ae7c351b6a666e32c1bae
Ge0frey <jeffkimzyjoe@gmail.com>
2025-01-07 01:09:03 +0300

    add endpoint as a constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,4 +5,6 @@ export const StablecoinData = {
   SIZE: 8 + 32 + 32 + 8 + 1 + (4 + 32) + (4 + 8) + (4 + 128) + (4 + 8)
 }; 
 
-export const PROGRAM_ID = new PublicKey("CGnwq4D9qErCRjPujz5MVkMaixR8BLRACpAmLWsqoRRe"); 
+export const PROGRAM_ID = new PublicKey("A6ZS2FHTzLuB6vP1XwDbb9TEtFdGZwT86dEJcGXmQPeU"); 
+
+export const RPC_ENDPOINT = "https://devnet.helius-rpc.com/?api-key=e26a41e3-3e82-45eb-956f-5a2160c31324"; 
