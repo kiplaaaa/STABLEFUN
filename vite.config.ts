34c556dbc40c15a617e3621d3d02d9679e2b7710
Ge0frey <jeffkimzyjoe@gmail.com>
2025-01-24 21:32:52 +0300

    run build successfully

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import polyfillNode from 'rollup-plugin-polyfill-node';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills({
+    // Whether to polyfill `node:` protocol imports.
+    protocolImports: true,
+  })],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      plugins: [polyfillNode()],
+    },
+  },
   resolve: {
     alias: {
+      crypto: 'crypto-browserify',
       stream: 'stream-browserify',
+      path: 'path-browserify',
+      os: 'os-browserify/browser',
+      fs: 'memfs',
       buffer: 'buffer',
       util: 'util',
       http: 'http-browserify'
